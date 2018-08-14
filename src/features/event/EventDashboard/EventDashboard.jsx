f4fae01631ce00b059b9af122d8fb571a1916d78
sudip <sudipzoom@rediffmail.com>
2018-08-14 20:42:46 +0530

    Form submit

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Grid,Button } from 'semantic-ui-react'
+import cuid from 'cuid';
 import EventList from '../EventList/EventList'
 import EventForm from '../EventForm/EventForm'
 
@@ -77,6 +78,17 @@ const eventsDashboard = [
   })
   }
 
+  handleCreateEvent = (newEvent)=>{
+  newEvent.id = cuid;
+  newEvent.hostPhotoURL = '/assets/user.png';
+  const updatedEvents = [...this.state.events, newEvent]
+  this.setState({
+    events:updatedEvents,
+     isOpen:false
+  })
+  }
+
+
   render() {
     return (
       <Grid>
@@ -85,8 +97,9 @@ const eventsDashboard = [
           </Grid.Column> 
           <Grid.Column width={6}>
           <Button onClick={ this.handleFormOpen} positive content='Create Event' />
-          {this.state.isOpen &&
-          <EventForm handleCancel = {this.handleCancel}/>}
+          {this.state.isOpen && (
+          <EventForm createEvent={this.handleCreateEvent} handleCancel = {this.handleCancel}/>
+          )}
           </Grid.Column>  
       </Grid>
     )
