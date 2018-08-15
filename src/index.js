fabe4bfb58b19b7bf855f14af2d18bd139bc40f2
sudip <sudipzoom@rediffmail.com>
2018-08-15 19:03:10 +0530

    Router implementation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import 'semantic-ui-css/semantic.min.css';
 import App from './app/layout/App';
@@ -22,5 +23,11 @@ import registerServiceWorker from './registerServiceWorker';
 
 // render(App);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+
+ <BrowserRouter>
+  <App />
+ </BrowserRouter>,
+
+document.getElementById('root'));
 registerServiceWorker();
