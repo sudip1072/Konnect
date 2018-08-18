1be85b85968e7cee9b3287f29aeba17bb1e08df4
sudip <sudipzoom@rediffmail.com>
2018-08-18 21:59:01 +0530

    ReduxForm

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux'
+import { reducer as FormReducer } from 'redux-form';
 import testReducer from '../../features/teastarea/testReducer';
 import eventReducer from '../../features/event/eventReducer'
 
 const rootReducer = combineReducers({
 
     test:testReducer,
-    events:eventReducer
+    events:eventReducer,
+    form: FormReducer
 
 })
 
